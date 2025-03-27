9ac85835f1c87d8a475c0f843c225543b81c6f87
mukundan1989 <128684032+mukundan1989@users.noreply.github.com>
2025-03-27 14:38:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Render! Your Flask app is running successfully."
+    return "Hello, Render! Your Flask app is running successfully. A quick change to test."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
